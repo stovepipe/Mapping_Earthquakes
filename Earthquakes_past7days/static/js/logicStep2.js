06effbfcd5fd0e33c175b6b688f35a42e16cb306
stovepipe <zach.mehlenbacher@gmail.com>
2022-04-05 21:51:22 -0400

    Adding markers and colors

--- Earthquakes_past7days/static/js/logicStep2.js
+++ Earthquakes_past7days/static/js/logicStep2.js
@@ -38,32 +38,12 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
   return {
     opacity: 1,
     fillOpacity: 1,
-    fillColor: getColor(feature.properties.mag),
+    fillColor: "#ffae42",
     color: "#000000",
-    radius: getRadius(feature.properties.mag),
+    radius: getRadius(),
     stroke: true,
     weight: 0.5
   };
-}
-
-// This function determines the color of the marker based on the magnitude of the earthquake.
-function getColor(magnitude) {
-  if (magnitude > 5) {
-    return "#ea2c2c";
-  }
-  if (magnitude > 4) {
-    return "#ea822c";
-  }
-  if (magnitude > 3) {
-    return "#ee9c00";
-  }
-  if (magnitude > 2) {
-    return "#eecc00";
-  }
-  if (magnitude > 1) {
-    return "#d4ee00";
-  }
-  return "#98ee00";
 }
 
 // This function determines the radius of the earthquake marker based on its magnitude.
@@ -76,18 +56,15 @@ function getRadius(magnitude) {
 }
 
 // Creating a GeoJSON layer with the retrieved data.
-L.geoJson(data, {
-    // We turn each feature into a circleMarker on the map.
-    pointToLayer: function(feature, latlng) {
-        console.log(data);
-        return L.circleMarker(latlng);
-      },
-    // We set the style for each circleMarker using our styleInfo function.
-  style: styleInfo,
-   // We create a popup for each circleMarker to display the magnitude and location of the earthquake
-   //  after the marker has been created and styled.
-   onEachFeature: function(feature, layer) {
-    layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
-  }
-}).addTo(map)
-});
+L.geoJSON(data, {
+
+  // We turn each feature into a circleMarker on the map.
+  
+  pointToLayer: function(feature, latlng) {
+              console.log(data);
+              return L.circleMarker(latlng);
+          },
+        // We set the style for each circleMarker using our styleInfo function.
+      style: styleInfo
+      }).addTo(map);
+  });
